e33563fab5cee45286747946ddfef8dc4407de4a
Denise Melo <nissemello@gmail.com>
2023-11-13 17:45:09 -0300

    commit

---
+++ bavk-end/DCCLOSET/src/index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import produtoController from './controller/produtoController.js'
+
+
+
+
+
+const servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+
+servidor.use(produtoController);
+
+
+
+
+
+
+const port = process.env.PORT;
+servidor.listen(port, ()=> console.log(`API subiu na porta ${port}`));
